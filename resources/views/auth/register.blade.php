3e0713c312bcbbc6a4d62486e7a171b559acd385
dgusgus <dgusgus1@gmail.com>
2020-05-23 09:07:47 -0500

    agregando usuarios

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,21 +12,35 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
+                            <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid 
-                                @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" 
+                                <input id="first_name" type="text" class="form-control @error('first_name') is-invalid 
+                                @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" 
                                 autofocus>
 
-                                @error('name')
+                                @error('first_name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
-                        
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control @error('username') is-invalid 
+                                @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" 
+                                autofocus>
+
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
